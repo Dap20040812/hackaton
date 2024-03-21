013a69f503201b8f61f146138cff371c26f3c837
Diego Prado <78450717+Dap20040812@users.noreply.github.com>
2024-03-21 11:39:59 -0500

    create interactive cards

--- src/components/project/ProjectStyle.jsx
+++ src/components/project/ProjectStyle.jsx
@@ -1,13 +1,14 @@
 import styled from "styled-components";
-import { PrimaryColor } from "../../assets/Color";
+import { PrimaryColor, SecondaryColor } from "../../assets/Color";
 
 export const Content = styled.div`
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: center;
-    width: 100%;
+    width: 95%;
     min-height: 30em;
+    gap: 2em;
 
     @media (max-width: 740px){
         flex-direction: column;
@@ -77,10 +78,10 @@ export const ContentColumn = styled.div`
     flex-direction: row;
     align-items: center;
     justify-content: center;
-    width: 100%;
+    width: 95%;
     min-height: 30em;
     gap: 2em;
-    margin-top: 5em;
+    margin-top: 2em;
 
     @media (max-width: 740px){
         flex-direction: column-reverse;
@@ -138,3 +139,107 @@ export const RightContentColumn = styled.div`
     }
 
 `;  
+
+export const FlipCard = styled.div`
+  background-color: transparent;
+  width: 100%;
+  height: 30em;
+
+  @media (max-width: 740px){
+    width: 90%;
+  }
+`;
+
+export const TitleCard = styled.h2`
+  font-size: 1.5em;
+  font-weight: 900;
+  text-align: center;
+  margin: 0;
+`;
+
+export const FlipCardInner = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+
+  ${FlipCard}:hover & {
+    transform: rotateY(180deg);
+  }
+`;
+
+export const FlipCardFront = styled.div`
+  box-shadow: 0 8px 14px 0 rgba(0,0,0,0.2);
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  border: 2px solid ${PrimaryColor};
+  border-radius: 1rem;
+  background: linear-gradient(120deg, ${SecondaryColor} 60%, ${SecondaryColor} 88%, ${SecondaryColor} 40%, ${PrimaryColor} 78%);
+`;
+
+export const FlipCardBack = styled.div`
+  box-shadow: 0 8px 14px 0 rgba(0,0,0,0.2);
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  border: 4px solid ${PrimaryColor};
+  border-radius: 1rem;
+  background: linear-gradient(120deg,${PrimaryColor} 30%,${PrimaryColor} 88%, ${PrimaryColor} 40%, ${SecondaryColor} 78%);
+  color: white;
+  transform: rotateY(180deg);
+
+    p {
+        width: 80%;
+    }
+`;
+
+export const FlipCardFront2 = styled.div`
+  box-shadow: 0 8px 14px 0 rgba(0,0,0,0.2);
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  border: 2px solid ${PrimaryColor};
+  border-radius: 1rem;
+  background: linear-gradient(120deg,${PrimaryColor} 30%,${PrimaryColor} 88%, ${PrimaryColor} 40%, ${SecondaryColor} 78%);
+`;
+
+export const FlipCardBack2 = styled.div`
+  box-shadow: 0 8px 14px 0 rgba(0,0,0,0.2);
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  border: 4px solid ${PrimaryColor};
+  border-radius: 1rem;
+  background: linear-gradient(120deg, ${SecondaryColor} 60%, ${SecondaryColor} 88%, ${SecondaryColor} 40%, ${PrimaryColor} 78%);
+  color: white;
+  transform: rotateY(180deg);
+
+    p {
+        width: 80%;
+    }
+`;
